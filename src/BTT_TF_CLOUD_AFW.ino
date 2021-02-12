312b948de9bd2f1360b7f6c273624fb681553cf2
Albrecht Lohofener <albrechtloh@gmx.de>
2021-02-12 01:41:21 +0100

    Add FTP server (WIP)

--- src/BTT_TF_CLOUD_AFW.ino
+++ src/BTT_TF_CLOUD_AFW.ino
@@ -1,14 +1,28 @@
-/* Using the WebDAV server
-	From windows - 
-		Run: \\HOSTNAME\DavWWWRoot
-		or Map Network Drive -> Connect to a Website
-*/
+/*
+ * Alternative firmware for BTT TF Cloud devices
+ * 
+ * 2021 - Albrecht Lohofener (albrechtloh@gmx.de)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <ESP8266WiFi.h>
 #include "ESPWebDAV.h"
+#include "ESPFtpServer.h"
 #include "WiFiSettings.h"
 
-#define HOSTNAME	"BTT_ESPWebDAV"
+#define HOSTNAME	"BTT_TF_CLOUD_AFW"
 #define SERVER_PORT	80
 #define SD_CS		5
 
@@ -16,13 +30,15 @@ ESPWebDAV dav;
 String statusMessage;
 bool initFailed = false;
 
+FtpServer ftpSrv;   //set #define FTP_DEBUG in ESP8266FtpServer.h to see ftp verbose on serial
+
 
 // ------------------------
 void setup() {
 // ------------------------
 	Serial.begin(115200);
 	Serial.println("");
-	Serial.println("Starting up");
+	Serial.println("Connecting to WiFi");
 
 	WiFi.mode(WIFI_STA);
 	WiFi.setPhyMode(WIFI_PHY_MODE_11N);
@@ -49,11 +65,12 @@ void setup() {
 		Serial.print("ERROR: "); Serial.println(statusMessage);
 		initFailed = true;
 	}
-
 	Serial.println("WebDAV server started");
+
+	// Start FTP server
+	ftpSrv.begin("anonymous","", SD_CS, SPI_FULL_SPEED);    //username, password for ftp.  set ports in ESP8266FtpServer.h  (default 21, 50009 for PASV)
+	Serial.println("FTP server started");
 }
-
-
 
 // ------------------------
 void loop() {
@@ -68,6 +85,6 @@ void loop() {
 		dav.initSD(SD_CS, SPI_FULL_SPEED);
 		dav.handleClient();
 	}
+
+	ftpSrv.handleFTP();        //make sure in loop you call handleFTP()!!  
 }
-
-
